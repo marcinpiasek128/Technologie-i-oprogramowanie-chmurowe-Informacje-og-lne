559279ae046131022b5bcea81f79ffe043c01961
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 09:12:52 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -4,5 +4,5 @@
 @$password='$LozVGJbooFxAK0N';
 @$database='calc';
 @$data='data';
-@$conn = mysqli_real_connect($server,$login,$passowrd,$database, 3306);
+@$conn = mysqli_real_connect($server,$login,$passowrd,$database, 3306) or die ("Wystąpił błąd połączenia z bazą danych");
 ?>
