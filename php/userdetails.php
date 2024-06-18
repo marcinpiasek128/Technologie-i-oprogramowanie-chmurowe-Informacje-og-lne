5c76595ac8d30797008bb58115bf37ad083395bd
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 19:39:50 +0200

    Update userdetails.php

--- php/userdetails.php
+++ php/userdetails.php
@@ -1,5 +1,6 @@
 <?php
 require("connect.php");
+session_start();
 
 $x = $_SESSION['ID_User'];
 $query = "SELECT Picture FROM data WHERE ID_User = ?";
@@ -10,9 +11,16 @@ if ($result === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
+$avatar = null;
 while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-    @$avatar = $row['Picture'];
+    $avatar = $row['Picture'];
 }
 
-echo '<img class="avatar" src="data:image/jpg;charset=utf8;base64,' . base64_encode($avatar) . '" />';
+if ($avatar !== null) {
+    echo '<img class="avatar" src="data:image/jpg;base64,' . base64_encode($avatar) . '" />';
+} else {
+    echo 'No image found for this user.';
+}
+
+sqlsrv_close($conn);
 ?>
