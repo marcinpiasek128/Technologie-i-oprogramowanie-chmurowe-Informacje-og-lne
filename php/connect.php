790a3d2c35e4f6e26b0ce5d0d56f1a2dab78279e
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 09:32:26 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -4,8 +4,8 @@
 @$password='';
 @$database='calc';
 @$conn = mysqli_init();
-mysqli_ssl_set($conn, NULL, NULL, "DigiCertGlobalRootCA.crt", NULL, NULL)
-mysqli_real_connect($conn, $server, $login, $passowrd, $database, 3306, MYSQL_CLIENT_SSL);
+mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, $server, $login, $passowrd, $database, 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) { 
   die('Nie udało się połączyć z bazą danych: '.mysqli_connect_errno()); 
 }
