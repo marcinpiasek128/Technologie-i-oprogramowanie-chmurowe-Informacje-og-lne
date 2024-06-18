4746fa441be6c2a3b3495f692d81e34029b2c748
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 18:26:18 +0200

    Update ban.php

--- php/ban.php
+++ php/ban.php
@@ -1,21 +1,18 @@
 <?php
 require("connect.php");
-if (isset($_POST["block"]))
-    {
-        $ban_id = $_POST['block'];
-        $sql = "UPDATE data SET ban='1' WHERE ID_User = '$ban_id'";
-        mysqli_query($conn,$sql);
 
-        if ($conn->query($sql) === TRUE) {
-            header("Location: adminpage.php");
-        } 
-        else 
-        {
-              
-        }
+if (isset($_POST["block"])) {
+    $ban_id = $_POST['block'];
+    $sql = "UPDATE data SET ban = '1' WHERE ID_User = ?";
+
+    // Prepare and execute the statement
+    $params = array($ban_id);
+    $stmt = sqlsrv_query($conn, $sql, $params);
+
+    if ($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        header("Location: adminpage.php");
     }
-    else
-    {
-        
-    }
+}
 ?>
