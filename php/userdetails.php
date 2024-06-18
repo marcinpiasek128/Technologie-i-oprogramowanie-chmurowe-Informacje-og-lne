b2f317d225b7de21aff04b542dd1163f140e4c2d
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 19:53:05 +0200

    Update userdetails.php

--- php/userdetails.php
+++ php/userdetails.php
@@ -1,35 +1,26 @@
 <?php
 require("connect.php");
+
 session_start();
-
-// Check if session variable is set
-if (!isset($_SESSION['ID_User'])) {
-    die('User not logged in.');
-}
-
 $x = $_SESSION['ID_User'];
 $query = "SELECT Picture FROM data WHERE ID_User = ?";
 $params = array($x);
-$result = sqlsrv_query($conn, $query, $params);
+$stmt = sqlsrv_query($conn, $query, $params);
 
-// Check for query errors
-if ($result === false) {
+if($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
-// Initialize avatar variable
-$avatar = null;
-while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     $avatar = $row['Picture'];
 }
 
-// Check if avatar is not null and display the image
-if ($avatar !== null) {
-    // Display the image
-    echo '<img class="avatar" src="data:image/jpg;base64,' . base64_encode($avatar) . '" />';
+if(isset($avatar)) {
+    echo '<img class="avatar" src="data:image/jpg;charset=utf8;base64,' . base64_encode($avatar) . '" />';
 } else {
-    echo 'No image found for this user.';
+    echo "Avatar not found.";
 }
 
+sqlsrv_free_stmt($stmt);
 sqlsrv_close($conn);
 ?>
