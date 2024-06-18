61bd3c1fe8911b306e4ebf22bfcc84f510fd35c4
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 18:49:58 +0200

    Update unban.php

--- php/unban.php
+++ php/unban.php
@@ -1,21 +1,20 @@
 <?php
 require("connect.php");
-if (isset($_POST["unblock"]))
-    {
-        $ban_id = $_POST['unblock'];
-        $sql = "UPDATE data SET ban='0' WHERE ID_User = '$ban_id'";
-        mysqli_query($conn,$sql);
 
-        if ($conn->query($sql) === TRUE) {
-            header("Location: adminpage.php");
-        } 
-        else 
-        {
-              
-        }
-    }
-    else
-    {
+if (isset($_POST["unblock"])) {
+    $ban_id = $_POST['unblock'];
+    $sql = "UPDATE data SET ban = 0 WHERE ID_User = ?";
 
+    // Prepare and execute the query
+    $stmt = sqlsrv_prepare($conn, $sql, array(&$ban_id));
+
+    if (sqlsrv_execute($stmt)) {
+        header("Location: adminpage.php");
+    } else {
+        // Error handling (if needed)
+        // echo "Error: " . sqlsrv_errors();
     }
+} else {
+    // Handle case where unblock is not set (if needed)
+}
 ?>
