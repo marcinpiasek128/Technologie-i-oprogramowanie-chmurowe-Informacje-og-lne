669b6e2ce6cc217787360aac1133ca615bf95337
DawidPiatek <83632802+DawidPiatek@users.noreply.github.com>
2024-06-18 21:12:59 +0200

    Update userdetails.php

--- php/userdetails.php
+++ php/userdetails.php
@@ -3,23 +3,25 @@ require("connect.php");
 
 session_start();
 $x = $_SESSION['ID_User'];
-$query = "SELECT Picture FROM data WHERE ID_User = '$x'";
-$stmt = sqlsrv_query($conn, $query);
+$query = "SELECT Picture FROM data WHERE ID_User = ?";
+$params = array($x);
+$stmt = sqlsrv_query($conn, $query, $params);
 
 if($stmt === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
+$avatar = null;
 while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
     $avatar = $row['Picture'];
 }
 
-if(isset($avatar)) {
-    echo '<img class="avatar" src="data:image/jpg;charset=utf8;base64,' . base64_encode($avatar) . '" />';
+sqlsrv_free_stmt($stmt);
+sqlsrv_close($conn);
+
+if($avatar !== null) {
+    echo '<img class="avatar" src="data:image/jpeg;base64,' . base64_encode($avatar) . '" />';
 } else {
     echo "Avatar not found.";
 }
-
-sqlsrv_free_stmt($stmt);
-sqlsrv_close($conn);
 ?>
