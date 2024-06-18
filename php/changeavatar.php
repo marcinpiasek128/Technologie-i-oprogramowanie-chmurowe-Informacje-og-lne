b1d3892cfe81d20256e588e75572af09b1409d4b
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 19:57:57 +0200

    Update changeavatar.php

--- php/changeavatar.php
+++ php/changeavatar.php
@@ -3,20 +3,21 @@ require("connect.php");
 session_start();
 $x = $_SESSION['ID_User'];
 
-if(!empty($_FILES["image"]["name"])) { 
-    $fileName = basename($_FILES["image"]["name"]); 
-    $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+if (!empty($_FILES["image"]["name"])) {
+    $fileName = basename($_FILES["image"]["name"]);
+    $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
     $allowTypes = array('jpg');
 
-    if(in_array($fileType, $allowTypes)){ 
-        $image = $_FILES['image']['tmp_name']; 
-        $avatar = file_get_contents($image); 
+    if (in_array($fileType, $allowTypes)) {
+        $image = $_FILES['image']['tmp_name'];
+        $avatar = file_get_contents($image);
 
-        $query = "UPDATE data SET Picture = ? WHERE ID_User = ?";
+        // Prepare the query with CONVERT function
+        $query = "UPDATE data SET Picture = CONVERT(varbinary(max), ?) WHERE ID_User = ?";
         $params = array($avatar, $x);
         $stmt = sqlsrv_query($conn, $query, $params);
 
-        if($stmt === false) {
+        if ($stmt === false) {
             die(print_r(sqlsrv_errors(), true));
         } else {
             header("Location: settings.php");
