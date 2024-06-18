f4ff17c17aec3ad87faef150c91366c6943369ee
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 18:46:23 +0200

    Update settings-script.php

--- php/settings-script.php
+++ php/settings-script.php
@@ -2,23 +2,28 @@
 session_start();
 require("connect.php");
 
-if ($conn->connect_errno!=0)
-{
-    echo "Error: ".$conn->connect_errno;
-}
-else
-{
-    if(isset($_POST['but_submit']))
-    {
-        @$login = $_POST['txt_uname'];
-        @$haslo = $_POST['txt_pwd'];
-        $fajna_zmienna=$_SESSION['ID_User'];
-        if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
-        {
-            $reg="UPDATE $data SET Username='$login', Password='$haslo' WHERE ID_User='$fajna_zmienna'";
-            $result=$conn->query($reg);
+// Check if connection was successful
+if ($conn === false) {
+    die("Error: " . print_r(sqlsrv_errors(), true));
+} else {
+    if (isset($_POST['but_submit'])) {
+        $login = $_POST['txt_uname'];
+        $haslo = $_POST['txt_pwd'];
+        $fajna_zmienna = $_SESSION['ID_User'];
+
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            $sql = "UPDATE data SET Username = ?, Password = ? WHERE ID_User = ?";
+            $params = array($login, $haslo, $fajna_zmienna);
+            $stmt = sqlsrv_query($conn, $sql, $params);
+
+            if ($stmt === false) {
+                die("Error executing query: " . print_r(sqlsrv_errors(), true));
+            }
+
+            sqlsrv_free_stmt($stmt);
         }
     }
 }
-$conn->close();
+
+sqlsrv_close($conn);
 ?>
