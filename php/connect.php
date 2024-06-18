a58bb51bd7e56ccc626ad69bc8474ff5b0c360e8
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 08:44:00 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,8 +1,8 @@
 <?php
-@$server='localhost';
-@$login='root';
-@$password='';
-@$database='moviehub';
+@$server='tiochkalkulator-sql.mysql.database.azure.com';
+@$login='wmdhxfuygy';
+@$password='$LozVGJbooFxAK0N';
+@$database='calc';
 @$data='data';
 @$conn=mysqli_connect($server,$login,$passowrd,$database) or die ("Wystąpił błąd połączenia z bazą danych");
 ?>
