2eb2cc5d356da18d249a4bfdb70cb71f29b1de65
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 18:52:56 +0200

    Update userdetails.php

--- php/userdetails.php
+++ php/userdetails.php
@@ -1,13 +1,18 @@
 <?php
-    require("connect.php");
+require("connect.php");
 
-    $x = $_SESSION['ID_User'];
-    $query = "SELECT Picture FROM data WHERE ID_User='$x'";
-    $result = $conn->query($query);
+$x = $_SESSION['ID_User'];
+$query = "SELECT Picture FROM data WHERE ID_User = ?";
+$params = array($x);
+$result = sqlsrv_query($conn, $query, $params);
 
-    while($row = $result->fetch_array())
-    {
-        @$avatar = $row['Picture'];
-    }
-    echo '<img class="avatar" src="data:image/jpg;charset=utf8;base64,'.base64_encode($avatar).'" />';
+if ($result === false) {
+    die(print_r(sqlsrv_errors(), true));
+}
+
+while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+    @$avatar = $row['Picture'];
+}
+
+echo '<img class="avatar" src="data:image/jpg;charset=utf8;base64,' . base64_encode($avatar) . '" />';
 ?>
