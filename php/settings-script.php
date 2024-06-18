135d36d636c813afbe8cc41451d80ffd87e15da1
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 16:49:53 +0200

    Update settings-script.php

--- php/settings-script.php
+++ php/settings-script.php
@@ -2,21 +2,19 @@
 session_start();
 require("connect.php");
 
-if ($conn->connect_errno!=0)
-{
-    echo "Error: ".$conn->connect_errno;
-}
-else
-{
-    if(isset($_POST['but_submit']))
-    {
-        @$login = $_POST['txt_uname'];
-        @$haslo = $_POST['txt_pwd'];
-        $fajna_zmienna=$_SESSION['ID_User'];
-        if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
-        {
-            $reg="UPDATE $data SET Username='$login', Password='$haslo' WHERE ID_User='$fajna_zmienna'";
-            $result=$conn->query($reg);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    if (isset($_POST['but_submit'])) {
+        $login = $_POST['txt_uname'];
+        $haslo = $_POST['txt_pwd'];
+        $fajna_zmienna = $_SESSION['ID_User'];
+
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+            $stmt = $conn->prepare("UPDATE data SET Username = ?, Password = ? WHERE ID_User = ?");
+            $stmt->bind_param("ssi", $login, $haslo, $fajna_zmienna);
+            $stmt->execute();
+            $stmt->close();
         }
     }
 }
