5b2f3e83bec04a196dbac965be76167ba010322c
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 19:55:42 +0200

    Update changeavatar.php

--- php/changeavatar.php
+++ php/changeavatar.php
@@ -3,36 +3,28 @@ require("connect.php");
 session_start();
 $x = $_SESSION['ID_User'];
 
-if (!empty($_FILES["image"]["name"])) {
-    $fileName = basename($_FILES["image"]["name"]);
-    $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+if(!empty($_FILES["image"]["name"])) { 
+    $fileName = basename($_FILES["image"]["name"]); 
+    $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
     $allowTypes = array('jpg');
 
-    if (in_array($fileType, $allowTypes)) {
-        $image = $_FILES['image']['tmp_name'];
-        $avatar = file_get_contents($image);
-        // Converting the image content to hexadecimal representation
-        $avatar = bin2hex($avatar);
+    if(in_array($fileType, $allowTypes)){ 
+        $image = $_FILES['image']['tmp_name']; 
+        $avatar = file_get_contents($image); 
 
-        $sql = "UPDATE data SET Picture = CONVERT(VARBINARY(MAX), ?) WHERE ID_User = ?";
+        $query = "UPDATE data SET Picture = ? WHERE ID_User = ?";
         $params = array($avatar, $x);
-        $stmt = sqlsrv_query($conn, $sql, $params);
+        $stmt = sqlsrv_query($conn, $query, $params);
 
-        if ($stmt === false) {
+        if($stmt === false) {
             die(print_r(sqlsrv_errors(), true));
         } else {
             header("Location: settings.php");
-            exit();
         }
-    } else {
-        // If file type is not allowed
-        $_SESSION['e_image'] = "Allowed file type is .jpg";
-        header("Location: settings.php");
-        exit();
     }
 } else {
     header("Location: settings.php");
-    exit();
 }
+
 sqlsrv_close($conn);
 ?>
