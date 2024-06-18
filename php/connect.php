ce695b6425a901c7f95196f2527cc26e48a80c24
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2024-06-18 16:38:29 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "kalkulatorwalut-server-admin", "pwd" => "!Tajn123", "Database" => "calc", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "kalkulatorwalut-server-admin", "pwd" => "!Tajne123", "Database" => "calc", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:kalkulatorwalut-server.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
